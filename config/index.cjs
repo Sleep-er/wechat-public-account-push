1755e327081d06346a81893604d6853a1ebaee09
Sleep-er <117742971+Sleep-er@users.noreply.github.com>
2022-11-08 22:16:18 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -21,7 +21,7 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '乖宝',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: 'oVkR_5_zFKe1iq8-nSzj0VP8PWvM',
+      id: 'oVkR_5w4nmXgsh4-c0ht5i1Eqcr8',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
       useTemplateId: 'yQ_fypUHgEEMBVlUk5JLJJhLL3h-U3qIFpY9Rv0lilY',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
